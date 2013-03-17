9d29ac4e855b6b690e7868ec5af5211388ba0fc6
Weston Platter <westonplatter@gmail.com>
2013-03-17 00:00:14 -0500

    the beginning of awesome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleTravisciMulti::Application.config.secret_token = 'd0dd4fd1dfa1150c697cfcf118edf058d36933cf005b897731ccf7711af88c86122a2711d9c8fabe157313426b2038dbfd09d0c8c4ec0a4cfa0ece23e13cfcb0'
